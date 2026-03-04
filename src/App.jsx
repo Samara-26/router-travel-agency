7f92c3091fb800eaac474d162c83d3f92d37b032
Samara <kyzy.samara@gmail.com>
2026-03-04 15:10:14 +0100

    4 pages connected

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   
   return (
-    <>
-    <p>Test to run!</p>
-    </>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/destinations' element={<Destinations />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/contact' element={<Contact />} />
+    </Routes>
   )
 }
 
