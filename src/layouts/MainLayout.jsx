9765ac1b51501dda7bc2912cb8913046159a8b0c
Samara <kyzy.samara@gmail.com>
2026-03-05 22:34:34 +0100

    data mapped, data is rendered

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,7 +3,9 @@ import { Outlet } from 'react-router'
 import NavBar from '../components/shared/NavBar'
 import Footer from '../components/shared/Footer'
 
+
 function MainLayout() {
+  
   return (
     <>
     <NavBar />
